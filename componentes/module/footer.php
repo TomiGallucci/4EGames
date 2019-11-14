f8f638bbd510342dbcccdd70b0ed207af5a1cb57
nachoramirez2306 <nachoramirez2306@gmail.com>
2019-11-13 23:17:15 -0300

    footer

--- componentes/module/footer.php
+++ componentes/module/footer.php
@@ -1,9 +1,25 @@
-    <footer>
-        <h3>4EGames</h3>
-        <ul>
-            <li>Home</li>
-            <li>Faq</li>
-            <li>Contacto</li>
-            <li>Catalogo</li>
-        </ul>
-    </footer>
+<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12 center">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12 center">
+                <h5 class="white-text">Social Media</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container center">
+            ©4EGames Copyrights
+            
+            </div>
+          </div>
+        </footer>
