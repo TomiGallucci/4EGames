d08c19562aebb0536e849e686bc6f404effe6704
Lucas Vergara <lukassvergara@gmail.com>
2019-11-18 02:23:18 -0300

    catalogo agregado

--- componentes/module/header.php
+++ componentes/module/header.php
@@ -4,14 +4,14 @@
     <div class="nav-wrapper">
       
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a class="" href="#" ><i class="material-icons left">account_box</i>Perfil</a></li>
+        <li><a class="" href="perfilusuario.php" ><i class="material-icons left">account_box</i>Perfil</a></li>
         <li><a href="#" class=""><i class="material-icons left">add_shopping_cart</i>Carrito</a></li>
       </ul>
       <ul id="nav-mobile" class="left hide-on-med-and-down">
         <li><a href="index.php">Home</a></li>
         <li><a href="faq.php">F.A.Q</a></li>
-        <li><a href="catalogo.html">Catalogo</a></li>
-        <li><a href="contacto.html">Contacto</a></li>
+        <li><a href="catalogo.php">Catalogo</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
       </ul>
     </div>
   </nav>
